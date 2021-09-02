53a97e9110cefc36c722145323b6ad6503f27503
Matthew Allison <mattymallison@gmail.com>
2021-09-02 15:37:54 +0200

    new code

--- app.py
+++ app.py
@@ -87,9 +87,9 @@ def signUp():
 
         if request.method == "POST":
 
-            name = request.json['name']
-            email = request.json['email']
-            password = request.json['password']
+            name = request.form['name']
+            email = request.form['email']
+            password = request.form['password']
 
             with sqlite3.connect("sneakeromatic.db") as conn:
                 cursor = conn.cursor()
